aae4cd295ed9ffb060afc12d888d1d5204c2c825
Sam Tebbs <samuel.tebbs@gmail.com>
2018-02-12 14:15:54 +0000

    Made bluetooth setup script a bit better

--- setup_bluetooth.sh
+++ setup_bluetooth.sh
@@ -1,19 +1,24 @@
+set -o xtrace -e
+echo "Installing library things that are supposed to be useful"
+sudo apt-get update
+sudo apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
+
+echo "Downloading and installing bluez v5.48"
 wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.48.tar.xz
 rm bluez-5.37.tar.xz
 tar xvf bluez-5.48.tar.xz
 cd bluez-5.48/
-sudo apt-get update
-sudo apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
 ./configure
 make
-make install
 sudo make install
+
+echo "Enabling bluetooth service"
 sudo systemctl enable bluetooth
 systemctl daemon-reload
-echo ""
 read -p "Add -C to the line containing bluetoothhd. Press enter when ready" null
 sudo nano /lib/systemd/system/bluetooth.service
+
+echo "Installing pyobjc and pybluez"
 pip install pyobjc
-pip install pybluez
 sudo apt-get install libbluetooth-dev
 sudo pip install pybluez
