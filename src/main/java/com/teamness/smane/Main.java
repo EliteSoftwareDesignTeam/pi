b71104198b8eb8c3eb3a219bade95f5495f62023
Sam Tebbs <samuel.tebbs@gmail.com>
2018-02-16 19:20:52 +0000

    Made Main class use python/example_server.py instead of python/process_test

--- src/main/java/com/teamness/smane/Main.java
+++ src/main/java/com/teamness/smane/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         ProcessRunner<String> btServer = new ProcessRunner<>();
         btServer.addHandler(String::new, System.out::println);
-        btServer.start(Arrays.asList("python", "python/process_test.py"));
+        btServer.start(Arrays.asList("python", "python/example_server.py"));
     }
 
 }
