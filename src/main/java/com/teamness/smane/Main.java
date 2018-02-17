25b61b90aa51d9b5324d695323350569937ddd08
Sam Tebbs <samuel.tebbs@gmail.com>
2018-02-17 16:19:23 +0000

    Added some event reception and transmission logic to Main

--- src/main/java/com/teamness/smane/Main.java
+++ src/main/java/com/teamness/smane/Main.java
@@ -1,15 +1,39 @@
 package com.teamness.smane;
 
+import com.teamness.smane.event.Event;
+import com.teamness.smane.event.EventChannel;
 import com.teamness.smane.process.ProcessRunner;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
 
+    public static EventChannel localEvents = new EventChannel(), bluetoothEvents = new EventChannel(), allEvents = new EventChannel(EventChannel.EventPriority.MEDIUM, EventChannel.EventPriority.HIGH, localEvents, bluetoothEvents);
+    private static ProcessRunner<Event> btServer;
+
     public static void main(String[] args) {
-        ProcessRunner<String> btServer = new ProcessRunner<>();
-        btServer.addHandler(String::new, System.out::println);
+        btServer = new ProcessRunner<>();
+        btServer.addHandler(b -> {
+            try {
+                return (Event) Serialisation.toObject(new String(b));
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }, Main::receiveEvent);
         btServer.start(Arrays.asList("sudo", "python", "python/cane-bluetooth.py"));
+        bluetoothEvents.onAny(EventChannel.EventPriority.HIGH, "sendEvent", null);
     }
+
+    public static void sendEvent(Event e) throws IOException {
+        btServer.send(Serialisation.fromObject(e));
+    }
+
+    public static void receiveEvent(Event e) {
+        bluetoothEvents.trigger(e);
+    }
+
+
 
 }
