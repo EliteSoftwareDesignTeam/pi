0cfebb1f18eb4cefac7a8df0c07a0050961140c8
Sam Tebbs <samuel.tebbs@gmail.com>
2018-02-17 14:50:12 +0000

    Reformatted cane-bluetooth.py

--- python/cane-bluetooth.py
+++ python/cane-bluetooth.py
@@ -1,40 +1,55 @@
-from bluetooth import *
+# file: rfcomm-server.py
+# auth: Albert Huang <albert@csail.mit.edu>
+# desc: simple demonstration of a server application that uses RFCOMM sockets
+#
+# $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
+from bluetooth import *
+import threading
+import sys
+
+def output(msg):
+    print msg
+    sys.stdout.flush()
+
+def stdin_loop():
+    while True:
+        input = sys.stdin.read()
+        print input
+
+server_sock = BluetoothSocket(RFCOMM)
+server_sock.bind(("", PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service( server_sock, "CanePi",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ],
-#                   protocols = [ OBEX_UUID ]
-                    )
-print "Advertising on UUID %s" % uuid
+advertise_service(server_sock, "SampleServer",
+                  service_id=uuid,
+                  service_classes=[uuid, SERIAL_PORT_CLASS],
+                  profiles=[SERIAL_PORT_PROFILE],
+                  #                   protocols = [ OBEX_UUID ]
+                  )
 
-while True:
-    print "Waiting for connection on RFCOMM channel %d" % port
-    client_sock, client_info = server_sock.accept()
-    print "Received connection from ", client_info
+output("Waiting for connection on RFCOMM channel %d" % port)
 
-    try:
+client_sock, client_info = server_sock.accept()
+output("Accepted connection")
+
+t1 = threading.Thread(target=stdin_loop, args=())
+t1.start()
+
+try:
+    while True:
         data = client_sock.recv(1024)
         if len(data) == 0: break
-        print "Received [%s]" % data
+        output("received [%s]" % data)
+except IOError:
+    pass
 
-        # Process
-        print "Sending [%s]" % data
-        #client_sock.send(data)
+output("disconnected")
 
-    except IOError:
-        pass
-
-    except KeyboardInterrupt:
-        print "Disconnected"
-        client_sock.close()
-        server_sock.close()
-        break
+client_sock.close()
+server_sock.close()
+output("all done")
