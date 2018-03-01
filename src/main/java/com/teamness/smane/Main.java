ac9ad21e0f8a5ff27b06827739785ad255f46682
Sam Tebbs <samuel.tebbs@gmail.com>
2018-03-01 15:43:16 +0000

    Added some testing stuff to Main

--- src/main/java/com/teamness/smane/Main.java
+++ src/main/java/com/teamness/smane/Main.java
@@ -1,15 +1,26 @@
 package com.teamness.smane;
 
+import com.teamness.smane.event.Event;
 import com.teamness.smane.process.ProcessRunner;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
-        ProcessRunner<String> btServer = new ProcessRunner<>();
-        btServer.addHandler(String::new, System.out::println);
-        btServer.start(Arrays.asList("sudo", "python", "python/cane-bluetooth.py"));
+    public static void main(String[] args)  {
+        ProcessRunner<Event> bluetoothProcess = new ProcessRunner<>();
+        bluetoothProcess.addHandler(bytes -> {
+            System.out.println("pls");
+            try {
+                System.out.println("Trying!");
+                return (Event) Serialisation.toObject(new String(bytes));
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }, s -> System.out.printf("Got event %s%n", s));
+        bluetoothProcess.start("sudo", "python", "python/cane-bluetooth.py");
     }
 
 }
