6c74d3f58c04771a2b302ca71db5fc29d3b219c9
Sam Tebbs <samuel.tebbs@gmail.com>
2018-03-03 17:42:16 +0000

    Added debug stuff to Main

--- src/main/java/com/teamness/smane/Main.java
+++ src/main/java/com/teamness/smane/Main.java
@@ -3,6 +3,7 @@ package com.teamness.smane;
 import com.teamness.smane.event.CaneEvents;
 import com.teamness.smane.event.Event;
 import com.teamness.smane.event.EventChannel;
+import com.teamness.smane.event.TestEvent;
 import com.teamness.smane.process.ProcessRunner;
 
 import java.io.IOException;
@@ -28,6 +29,7 @@ public class Main {
         btServer = new ProcessRunner<>();
         btServer.addHandler(b -> {
             String str = new String(b);
+            System.out.printf("Got %s from pi%n", str);
             if(str.charAt(str.length()-1) == '\n') str = str.substring(0, str.length()-1);
             try {
                 return (Event) Serialisation.toObject(str);
@@ -36,15 +38,19 @@ public class Main {
             }
             return null;
         }, Main::receiveEvent);
+        btServer.setRedirectError(false);
+        btServer.setErrHandler(s -> System.out.printf("Got error from pi: %s%n", s));
         btServer.start(Arrays.asList("sudo", "python", "python/cane-bluetooth.py"));
         CaneEvents.BT_OUT.onAny(EventChannel.EventPriority.HIGH, "sendEvent", Main.class);
     }
 
     public static void sendEvent(Event e) throws IOException {
+        System.out.println("Sending");
         btServer.send(Serialisation.fromObject(e));
     }
 
     public static void receiveEvent(Event e) {
+        System.out.printf("Got event %s%n", e);
         CaneEvents.BT_IN.trigger(e);
     }
 
