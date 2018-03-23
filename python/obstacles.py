fac01a03c2b4573d629626fba9fb8e3caff332cf
Sam Tebbs <samuel.tebbs@gmail.com>
2018-03-23 12:48:10 +0000

    thigns v2

--- python/obstacles.py
+++ python/obstacles.py
@@ -21,19 +21,17 @@ while True:
     GPIO.output(TRIG, False)
 
     while GPIO.input(ECHO)==0:
-        pass
-
-    pulse_start = time.time()
+        pulse_start = time.time()
 
     while GPIO.input(ECHO)==1:
-        pass
+        pulse_end = time.time()
 
-    pulse_duration = time.time() - pulse_start
+    pulse_duration = pulse_end - pulse_start
 
     distance = pulse_duration * 17150
     distance = round(distance, 2)
 
-    if 2 < distance < 400:
+    if distance > 2 and distance < 400:
         print "Distance:",distance - 0.5,"cm"
     else:
         print "Out Of Range"
